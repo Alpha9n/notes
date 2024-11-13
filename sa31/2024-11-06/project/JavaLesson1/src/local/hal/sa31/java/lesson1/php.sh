3635fda0939632c666e50b523c0126fe8b5d0713
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-11-13 16:51:03 +0900

    add: 課題提出用の修正

---
+++ sa31/2024-11-06/project/JavaLesson1/src/local/hal/sa31/java/lesson1/php.sh
@@ -0,0 +1,23 @@
+echo "===PHP実行==="
+
+echo "\n=====ex1:basicSyntax.php"
+sleep 5
+php ./ex1/basicSyntax.php
+sleep 2
+
+echo "\n=====ex2:breakAndContinue.php"
+sleep 5
+php ./ex2/breakAndContinue.php
+sleep 2
+
+echo "\n=====ex3:useNameAndAge.php"
+sleep 5
+php ./ex3/useNameAndAge.php
+sleep 2
+
+echo "\n=====ex4:calcFromRadius.php"
+sleep 5
+php ./ex4/calcFromRadius.php
+sleep 2
+
+echo "===終了==="
