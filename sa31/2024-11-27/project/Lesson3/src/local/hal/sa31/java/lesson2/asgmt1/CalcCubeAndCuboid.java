d9400b5a0318bac6ed8be732880e8a7ee4242f74
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-11-28 14:56:48 +0900

    add: ノート

---
+++ sa31/2024-11-27/project/Lesson3/src/local/hal/sa31/java/lesson2/asgmt1/CalcCubeAndCuboid.java
@@ -0,0 +1,61 @@
+package local.hal.sa31.java.lesson2.asgmt1;
+
+/**
+ * SA31 課題3
+ *
+ * フィールドに格納された各辺の値をもとに表面積と体積を計算するクラス
+ *
+ * @author kosuke Jimbo
+ */
+public class CalcCubeAndCuboid {
+    /**
+     * 高さを表すフィールド
+     */
+    private int _height;
+    /**
+     * 幅(横の長さ)を表すフィールド
+     */
+    private int _width;
+    /**
+     * 奥行き(縦の長さ)を表すフィールド
+     */
+    private int _depth;
+
+    /**
+     * コンストラクタ
+     * @param height 高さ
+     * @param width 幅
+     * @param depth 奥行き
+     */
+    public CalcCubeAndCuboid(int height, int width, int depth) {
+        _height = height;
+        _width = width;
+        _depth = depth;
+    }
+
+    /**
+     * コンストラクタ
+     * @param side 1辺の長さ
+     */
+    public CalcCubeAndCuboid(int side) {
+        _height = side;
+        _width = side;
+        _depth = side;
+    }
+
+    /**
+     * 直方体、もしくは、立方体の表面積を取得するメソッド
+     * @return 表面積
+     */
+    public int getArea() {
+        return 2 * ( _height * _width + _height * _depth + _width * _depth );
+    }
+
+    /**
+     * 直方体、もしくは、立方体の体積を取得するメソッド
+     * @return 体積
+     */
+    public int getVolume() {
+        return _height * _width * _depth;
+    }
+}
